1a7f4fb3074b62c51a702f01cbf227347e9a9ac4
Stein-N <nico-stein-98@outlook.de>
2024-07-11 17:02:10 +0200

    neoforge: splitting every handler into separate class

--- neoforge/src/testmod/java/net/xstopho/resourcelibrary_test/handler/NeoForgeClientHandler.java
+++ neoforge/src/testmod/java/net/xstopho/resourcelibrary_test/handler/NeoForgeClientHandler.java
@@ -1,4 +1,10 @@
 package net.xstopho.resourcelibrary_test.handler;
 
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.xstopho.resourcelibrary_test.ResourceLibraryTest;
+
+@EventBusSubscriber(modid = ResourceLibraryTest.MOD_ID, value = Dist.CLIENT)
 public class NeoForgeClientHandler {
+
 }
--- neoforge/src/testmod/java/net/xstopho/resourcelibrary_test/handler/NeoForgeHandler.java
+++ neoforge/src/testmod/java/net/xstopho/resourcelibrary_test/handler/NeoForgeHandler.java
@@ -1,4 +1,8 @@
 package net.xstopho.resourcelibrary_test.handler;
 
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.xstopho.resourcelibrary_test.ResourceLibraryTest;
+
+@EventBusSubscriber(modid = ResourceLibraryTest.MOD_ID, bus = EventBusSubscriber.Bus.GAME)
 public class NeoForgeHandler {
 }
