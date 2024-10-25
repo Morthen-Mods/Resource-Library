effad295ed87592d4e03943d2ec3861b8f66c47d
Stein-N <nico-stein-98@outlook.de>
2024-10-25 17:52:35 +0200

    fix id not set issue for now

--- fabric/src/testmod/java/net/xstopho/resourcelibrary_test/registries/BlockRegistry.java
+++ fabric/src/testmod/java/net/xstopho/resourcelibrary_test/registries/BlockRegistry.java
@@ -18,7 +18,9 @@ import java.util.function.Supplier;
 public class BlockRegistry {
     public static final RegistryProvider<Block> BLOCKS = RegistryProvider.get(ResourceLibraryTest.MOD_ID, BuiltInRegistries.BLOCK);
 
-    public static final RegistryObject<Block> TEST_FURNACE_LIKE_BLOCK = register("furnace_like_block", () -> new FurnaceBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.FURNACE)));
+    public static final RegistryObject<Block> TEST_FURNACE_LIKE_BLOCK = register("furnace_like_block",
+            () -> new FurnaceBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.FURNACE)
+                    .setId(ResourceKey.create(BuiltInRegistries.BLOCK.key(), ResourceLocation.fromNamespaceAndPath(ResourceLibraryTest.MOD_ID, "furnace_like_block")))));
 
     public static RegistryObject<Block> register(String id, Supplier<Block> block) {
         RegistryObject<Block> toReturn = BLOCKS.register(id, block);
@@ -27,7 +29,7 @@ public class BlockRegistry {
     }
 
     public static RegistryObject<Block> register(String id) {
-        return register(id, () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.STONE)));
+        return register(id, () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.STONE).setId(ResourceKey.create(BuiltInRegistries.BLOCK.key(), ResourceLocation.fromNamespaceAndPath(ResourceLibraryTest.MOD_ID, id)))));
     }
 
     public static void init() {}
--- neoforge/src/testmod/java/net/xstopho/resourcelibrary_test/registries/BlockRegistry.java
+++ neoforge/src/testmod/java/net/xstopho/resourcelibrary_test/registries/BlockRegistry.java
@@ -18,16 +18,20 @@ import java.util.function.Supplier;
 public class BlockRegistry {
     public static final RegistryProvider<Block> BLOCKS = RegistryProvider.get(ResourceLibraryTest.MOD_ID, BuiltInRegistries.BLOCK);
 
-    public static final RegistryObject<Block> FURNACE_LIKE_BLOCK = register("furnace_like_block", () -> new FurnaceBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.FURNACE)));
+    public static final RegistryObject<Block> FURNACE_LIKE_BLOCK = register("furnace_like_block",
+            () -> new FurnaceBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.FURNACE)
+                    .setId(ResourceKey.create(BuiltInRegistries.BLOCK.key(), ResourceLocation.fromNamespaceAndPath(ResourceLibraryTest.MOD_ID, "furnace_like_block")))));
 
     public static RegistryObject<Block> register(String id, Supplier<Block> block) {
         RegistryObject<Block> toReturn = BLOCKS.register(id, block);
-        ItemRegistry.register(id, () -> new BlockItem(toReturn.get(), new Item.Properties().setId(ResourceKey.create(BuiltInRegistries.ITEM.key(), ResourceLocation.fromNamespaceAndPath(ResourceLibraryTest.MOD_ID, id)))));
+        ItemRegistry.register(id, () -> new BlockItem(toReturn.get(), new Item.Properties()
+                .setId(ResourceKey.create(BuiltInRegistries.ITEM.key(), ResourceLocation.fromNamespaceAndPath(ResourceLibraryTest.MOD_ID, id)))));
         return toReturn;
     }
 
     public static RegistryObject<Block> register(String id) {
-        return register(id, () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.STONE)));
+        return register(id, () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.STONE)
+                .setId(ResourceKey.create(BuiltInRegistries.BLOCK.key(), ResourceLocation.fromNamespaceAndPath(ResourceLibraryTest.MOD_ID, id)))));
     }
 
     public static void init() {}
