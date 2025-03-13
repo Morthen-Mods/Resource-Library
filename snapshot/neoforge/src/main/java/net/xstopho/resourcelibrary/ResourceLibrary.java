8522f95a5b2770487969828c74e1bd923a548df0
Stein-N <nico-stein-98@outlook.de>
2025-03-13 17:05:16 +0100

    adding a snapshot subproject for faster updating

---
+++ snapshot/neoforge/src/main/java/net/xstopho/resourcelibrary/ResourceLibrary.java
@@ -0,0 +1,12 @@
+package net.xstopho.resourcelibrary;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(LibConstants.MOD_ID)
+public class ResourceLibrary {
+
+    public ResourceLibrary(IEventBus eventBus) {
+
+    }
+}
