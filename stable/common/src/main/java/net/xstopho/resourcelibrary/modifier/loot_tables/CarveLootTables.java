b394ce36fd399acf09de73ff52c7bc314ba23f1e
Stein-N <nico-stein-98@sbvl.de>
2026-06-24 19:34:21 +0200

    update gradle setup

--- stable/common/src/main/java/net/xstopho/resourcelibrary/modifier/loot_tables/CarveLootTables.java
+++
@@ -1,9 +0,0 @@
-package net.xstopho.resourcelibrary.modifier.loot_tables;
-
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.level.storage.loot.LootTable;
-import net.xstopho.resourcelibrary.LibConstants;
-
-public class CarveLootTables {
-    public static final ResourceKey<LootTable> PUMPKIN = LibConstants.createLootTableKey("carve/pumpkin");
-}
