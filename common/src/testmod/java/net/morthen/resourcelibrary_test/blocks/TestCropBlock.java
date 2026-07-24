c88c0be8f12d88636bf4f4dcca2b72a027939e8c
Stein-N <nico-stein-98@sbvl.de>
2026-07-24 12:52:12 +0200

    update gradle setup

--- common/src/testmod/java/net/morthen/resourcelibrary_test/blocks/TestCropBlock.java
+++
@@ -1,9 +0,0 @@
-package net.morthen.resourcelibrary_test.blocks;
-
-import net.minecraft.world.level.block.CropBlock;
-
-public class TestCropBlock extends CropBlock {
-    public TestCropBlock(Properties properties) {
-        super(properties);
-    }
-}
