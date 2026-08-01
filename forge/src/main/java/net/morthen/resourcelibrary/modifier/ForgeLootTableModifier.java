4c4c2f404f8edfde0ae52f16aeb2bec2482eeb11
Stein-N <nico-stein-98@sbvl.de>
2026-08-01 10:34:24 +0200

    re add Constant Amount loot pool builder and change the modifier record for neo-/forge

--- forge/src/main/java/net/morthen/resourcelibrary/modifier/ForgeLootTableModifier.java
+++ forge/src/main/java/net/morthen/resourcelibrary/modifier/ForgeLootTableModifier.java
@@ -15,22 +15,29 @@ import java.util.function.Supplier;
 
 @Mod.EventBusSubscriber(modid = LibConstants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ForgeLootTableModifier implements LootTableModifier {
-    private record Modifier(ItemLike item, ResourceKey<LootTable> table, Supplier<Float> chance, float minAmount, float maxAmount) {}
+    private record Modifier(ResourceKey<LootTable> lootTable, LootPool lootPool) {}
+
     private static final List<Modifier> modifiers = new ArrayList<>();
 
     @Override
+    public void addItem(ItemLike item, float amount, Supplier<Float> chance, List<ResourceKey<LootTable>> lootTables) {
+        for (ResourceKey<LootTable> lootTable : lootTables) {
+            modifiers.add(new Modifier(lootTable, LootTableModifier.lootPool(item, chance.get(), amount).build()));
+        }
+    }
+
+    @Override
     public void addItem(ItemLike item, float minAmount, float maxAmount, Supplier<Float> chance, List<ResourceKey<LootTable>> lootTables) {
-        for (ResourceKey<LootTable> table : lootTables) {
-            modifiers.add(new Modifier(item, table, chance, minAmount, maxAmount));
+        for (ResourceKey<LootTable> lootTable : lootTables) {
+            modifiers.add(new Modifier(lootTable, LootTableModifier.lootPool(item, chance.get(), minAmount, maxAmount).build()));
         }
     }
 
     @SubscribeEvent
     public static void loadTables(LootTableLoadEvent event) {
         modifiers.forEach(modifier -> {
-            if (event.getName().equals(modifier.table().identifier()) && event.getTable() != null) {
-                LootPool pool = LootTableModifier.lootPool(modifier.item(), modifier.chance().get(), modifier.minAmount(), modifier.maxAmount()).build();
-                event.getTable().addPool(pool);
+            if (event.getName().equals(modifier.lootTable().identifier())) {
+                event.getTable().addPool(modifier.lootPool());
             }
         });
     }
--- neoforge/src/main/java/net/morthen/resourcelibrary/modifier/NeoforgeLootTableModifier.java
+++ neoforge/src/main/java/net/morthen/resourcelibrary/modifier/NeoforgeLootTableModifier.java
@@ -15,22 +15,29 @@ import java.util.function.Supplier;
 
 @EventBusSubscriber(modid = LibConstants.MOD_ID)
 public class NeoforgeLootTableModifier implements LootTableModifier {
-    private record Modifier(ItemLike item, ResourceKey<LootTable> table, Supplier<Float> chance, float minAmount, float maxAmount) {}
+    private record Modifier(ResourceKey<LootTable> lootTable, LootPool lootPool) {}
+
     private static final List<Modifier> modifiers = new ArrayList<>();
 
     @Override
+    public void addItem(ItemLike item, float amount, Supplier<Float> chance, List<ResourceKey<LootTable>> lootTables) {
+        for (ResourceKey<LootTable> lootTable : lootTables) {
+            modifiers.add(new Modifier(lootTable, LootTableModifier.lootPool(item, chance.get(), amount).build()));
+        }
+    }
+
+    @Override
     public void addItem(ItemLike item, float minAmount, float maxAmount, Supplier<Float> chance, List<ResourceKey<LootTable>> lootTables) {
-        for (ResourceKey<LootTable> table : lootTables) {
-            modifiers.add(new Modifier(item, table, chance, minAmount, maxAmount));
+        for (ResourceKey<LootTable> lootTable : lootTables) {
+            modifiers.add(new Modifier(lootTable, LootTableModifier.lootPool(item, chance.get(), minAmount, maxAmount).build()));
         }
     }
 
     @SubscribeEvent
     public static void loadTables(LootTableLoadEvent event) {
         modifiers.forEach(modifier -> {
-            if (event.getName().equals(modifier.table().identifier())) {
-                LootPool pool = LootTableModifier.lootPool(modifier.item(), modifier.chance().get(), modifier.minAmount(), modifier.maxAmount()).build();
-                event.getTable().addPool(pool);
+            if (event.getName().equals(modifier.lootTable().identifier())) {
+                event.getTable().addPool(modifier.lootPool());
             }
         });
     }
